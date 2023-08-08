5e3bf88eb6584e23684b8d9ee95e3daa83c40715
John Doe <johndoe@email.com>
2023-08-08 22:41:49 +0200

    08-aug

---
+++ src/components/TextForms.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TextForms(props) {
+  return (
+    <div>
+        <h1>{props.heading}</h1>
+        <div class="mb-3">
+        
+        <textarea class="form-control" id="myBox" rows="8"></textarea>
+        </div>
+
+    </div>
+  )
+}
---
+++ src/module2.mjs
@@ -0,0 +1,7 @@
+const a = "Maaz";
+const b = "Sufian";
+const c = "Saad";
+const d = "Umar";
+
+export default c;
+export {a,b,d};
