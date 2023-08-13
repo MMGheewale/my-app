b811d7dbe81ddf7c96a9449239cc9663f13a168a
John Doe <johndoe@email.com>
2023-08-13 18:47:09 +0200

    13-AUG

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForms from './components/TextForms';
+// import TextForms from './components/TextForms';
+import About from './components/About';
+
 
  function App() {
   return (
    <>
    <Navbar title="Dropin" navbarcontent2="About"/>
    <div className="container my-3">
-      <TextForms heading="Enter the text to analyse below"/>
-      </div>
-      <button className="btn btn-primary">Convert to upper case</button>
-   
+   {/* <TextForms heading="Enter the text to analyse below"/> */}
+   <About/>
+   </div>
    </>
   );
 }
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 export default function TextForms(props) {
-  return (
-    <div>
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const handleDownClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleclearClick = ()=>{
+        let newText = "";
+        setText(newText)
+    }
+    const handleOnChange = (event)=>{
+        setText(event.target.value)
+    }
+    
+    const [text, setText] = useState('');
+    
+    return (
+        <>
+    <div className='container'>
         <h1>{props.heading}</h1>
-        <div class="mb-3">
-        
-        <textarea class="form-control" id="myBox" rows="8"></textarea>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to upper case</button>
+        <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to lower case</button>
+        <button className="btn btn-primary mx-2" onClick={handleclearClick}>Clear Text</button>
+    </div>
+    <div className="container my-3">
+        <h2>Your text summary</h2>
+        <b><p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008*text.split(" ").length} Minutes reading time for user</p></b>
+        <h2>Preview</h2>
+        <p>{text}</p>
 
     </div>
+    </>
   )
 }
